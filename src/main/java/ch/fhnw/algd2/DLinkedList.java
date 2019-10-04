e32f5fe7b3d05aeb60768a333ec7eec1a029440d
Mike Nöthiger <noethiger.mike@gmail.com>
2019-10-04 18:23:10 +0200

    assign functions to implement

--- src/main/java/ch/fhnw/algd2/DLinkedList.java
+++ src/main/java/ch/fhnw/algd2/DLinkedList.java
@@ -33,17 +33,27 @@ public class DLinkedList<T> extends AbstractList<T> implements IList<T> {
 
     @Override
     public boolean addAll(Collection<? extends T> c) {
-        return super.addAll(c);
+        if (c == null || c.isEmpty()) return false;
+        for (T item : c) {
+            ListItem<T> lItem = new ListItem<>(item);
+            linkInBack(lItem);
+        }
+        return true;
     }
 
     @Override
     public T get(ListItem item) {
-        return null;
+        // TODO find solution for type check on item
+        if (item == null || !checkMembership(item)) {
+            throw new NoSuchElementException();
+        }
+
+        return (T) item.m_data;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        throw new UnsupportedOperationException("unsupported");
     }
 
     @Override
@@ -53,117 +63,144 @@ public class DLinkedList<T> extends AbstractList<T> implements IList<T> {
 
     @Override
     public boolean checkMembership(ListItem item) {
-        return false;
+        // TODO in order to check the membership of an item, the item needs a reference to the LinkedList it belongs to. This reference has to be removed (or set to null), as the item is deleted from the list.
+        throw new UnsupportedOperationException("not yet implemented");
     }
 
     @Override
     public ListItem head() {
-        return null;
+        return m_head;
     }
 
     @Override
     public ListItem tail() {
-        return null;
+        return m_tail;
     }
 
     @Override
     public ListItem next(ListItem item) {
-        return null;
+        if (!checkMembership(item)) {
+            throw new NoSuchElementException(item.toString());
+        }
+        return item.m_next;
     }
 
     @Override
     public ListItem previous(ListItem item) {
-        return null;
+        if (!checkMembership(item)) {
+            throw new NoSuchElementException(item.toString());
+        }
+        return item.m_previous;
     }
 
     @Override
     public ListItem cyclicNext(ListItem item) {
-        return null;
+        if (!checkMembership(item)) {
+            throw new NoSuchElementException(item.toString());
+        }
+        if (item == m_tail) {
+            return m_head;
+        }
+        return item.m_next;
     }
 
     @Override
     public ListItem cyclicPrevious(ListItem item) {
-        return null;
+        if (!checkMembership(item)) {
+            throw new NoSuchElementException(item.toString());
+        }
+        if (item == m_head) {
+            return m_tail;
+        }
+        return item.m_previous;
     }
 
     @Override
     public ListItem delete(ListItem item, boolean next) {
+        // TODO @sämi
         return null;
     }
 
     @Override
     public ListItem cyclicDelete(ListItem item, boolean next) {
+        // TODO @sämi
         return null;
     }
 
     @Override
     public void set(ListItem item, T data) {
-
+        // TODO @marco
     }
 
     @Override
     public T remove(ListItem item) {
+        // TODO @sämi
         return null;
     }
 
     @Override
     public ListItem addHead(T data) {
+        // TODO @mike
         return null;
     }
 
     @Override
     public ListItem addTail(T data) {
+        // TODO @mike
         return null;
     }
 
     @Override
     public ListItem addAfter(ListItem item, T data) {
+        // TODO @mike
         return null;
     }
 
     @Override
     public ListItem addBefore(ListItem item, T data) {
+        // TODO @mike
         return null;
     }
 
     @Override
     public void moveToHead(ListItem item) {
+        // TODO @sämi
 
     }
 
     @Override
     public void moveToTail(ListItem item) {
-
+        // TODO @sämi
     }
 
     @Override
     public void rotate(ListItem item) {
-
+        // TODO @marco
     }
 
     @Override
     public void swap(ListItem item1, ListItem item2) {
-
+        // TODO @mike
     }
 
     @Override
     public void reverse() {
-
+        // TODO @mike
     }
 
     @Override
     public void addAfter(ListItem item, List<T> list) {
-        // TODO list has to be copied into ListItem's
+        // TODO @sämi list has to be copied into ListItem's
     }
 
     @Override
     public void addBefore(ListItem item, List<T> list) {
-        // TODO list has to be copied into ListItem's
+        // TODO @sämi list has to be copied into ListItem's
     }
 
     @Override
     public void conc(List<T> list, boolean after) {
-
+        // TODO @sämi
     }
 
     @Override
